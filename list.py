688c2f2a990eb6766cf55499555d8f1c348b71d0
Baanbil Godfred <atdevbills@gmail.com>
2026-04-08 10:10:15 +0000

    list

---
+++ list.py
@@ -0,0 +1,6 @@
+fruits = ["mango", "orange", "pineapple", "banana"]
+
+print(fruits)           # print entire list
+print(fruits[0])        # first item
+print(fruits[-1])       # last item
+print(len(fruits))      # number of items
